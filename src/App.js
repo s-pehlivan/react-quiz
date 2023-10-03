dd9bce1b254e67a9f0352e05e42057ccbbbc92e7
F. Sevde Pehlivan <109543298+s-pehlivan@users.noreply.github.com>
2023-10-03 14:41:52 +0300

    use json-serve to fetch questions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
 
-import DateCounter from "./components/DateCounter";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen";
+import Question from "./components/Question";
+
+const initialState = {
+  questions: [],
+  // "loading", "error", "ready", "active", "finished"
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Unexpected action type.");
+  }
+}
 
 const App = () => {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const numQuestions = questions.length;
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "active" && <Question />}
+      </Main>
     </div>
   );
 };
